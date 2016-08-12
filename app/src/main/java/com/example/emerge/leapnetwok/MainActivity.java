353d33097afba3797faea443ddb3d83aa42cd19a
chenhao-company <chenhao9255@gmail.com>
2016-08-12 15:52:18 +0800

    store wifi and BT in preference

--- app/src/main/java/com/example/emerge/leapnetwok/MainActivity.java
+++ app/src/main/java/com/example/emerge/leapnetwok/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.emerge.leapnetwok;
 
+import android.app.PendingIntent;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothClass;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 
@@ -19,12 +25,68 @@ import com.amap.api.location.AMapLocationClient;
 import com.amap.api.location.AMapLocationClientOption;
 import com.amap.api.location.AMapLocationListener;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button mTrackBtn;
     private WifiManager wifiManager;
     List<ScanResult> scanWifiResultList;
+    private String WIFI_PREF = "wifi_pref";
+    private String BLUETOOTH_PREF = "bluetooth_pref";
+    private String GEOFENCE_BROADCAST_ACTION = "GEOFENCE_BROADCAST_ACTION";
+    private List<MyBluetoothDevice> mBluetoothDeviceList = new ArrayList<MyBluetoothDevice>();
+
+    class MyBluetoothDevice{
+        public  BluetoothDevice mBluetoothDevice;
+        public  int mRssi;
+    }
+
+    private BroadcastReceiver mReceiver = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            String action = intent.getAction();
+            // 获得已经搜索到的蓝牙设备
+            if (action.equals(BluetoothDevice.ACTION_FOUND)) {
+                BluetoothDevice device = intent
+                        .getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                int rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI,Short.MIN_VALUE);
+                if (device.getBondState() != BluetoothDevice.BOND_BONDED) {
+                    Log.d("chenhao", device.getName() + " " + device.getAddress() + " " + rssi);
+                    MyBluetoothDevice myBluetoothDevice = new MyBluetoothDevice();
+                    myBluetoothDevice.mBluetoothDevice = device;
+                    myBluetoothDevice.mRssi = rssi;
+                    mBluetoothDeviceList.add(myBluetoothDevice);
+                }
+            } else if (action.equals(BluetoothAdapter.ACTION_DISCOVERY_FINISHED)) {
+                SharedPreferences.Editor editor = getSharedPreferences(BLUETOOTH_PREF,0).edit();
+
+                SimpleDateFormat sDateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+                String date = sDateFormat.format(new java.util.Date());
+
+                for(MyBluetoothDevice device : mBluetoothDeviceList){
+                    editor.putInt(date + " | " + device.mBluetoothDevice.getName() + " | " +
+                            device.mBluetoothDevice.getAddress(), device.mRssi);
+                }
+
+                editor.commit();
+
+                Toast.makeText(getApplicationContext(), "scan BT done", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+
+    private BroadcastReceiver mGeoFenceReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+        }
+    };
+
 
     //声明AMapLocationClient类对象
     public AMapLocationClient mLocationClient = null;
@@ -53,9 +115,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        scanWifi();
-        scanBluetooth();
-        setupLBS();
+        mTrackBtn = (Button)findViewById(R.id.track_id);
+        mTrackBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                scanWifi();
+                scanBluetooth();
+                //setupLBS();
+            }
+        });
 
         // 注册用以接收到已搜索到的蓝牙设备的receiver
         IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
@@ -75,6 +143,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         unregisterReceiver(mReceiver);
+        unregisterReceiver(mGeoFenceReceiver);
 
         mLocationClient.stopLocation();
         mLocationClient.onDestroy();
@@ -87,30 +156,45 @@ public class MainActivity extends AppCompatActivity {
         mLocationClient.setLocationListener(mLocationListener);
 
         mLocationOption = new AMapLocationClientOption();
-        //设置定位模式为AMapLocationMode.Hight_Accuracy，高精度模式。
         mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
         mLocationOption.setOnceLocationLatest(true);
         mLocationOption.setOnceLocationLatest(true);
         mLocationOption.setWifiActiveScan(true);
-
-        //给定位客户端对象设置定位参数
         mLocationClient.setLocationOption(mLocationOption);
+
+        // Geo Fence
+        /*IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        filter.addAction(GEOFENCE_BROADCAST_ACTION);
+        registerReceiver(mGeoFenceReceiver,filter);
+
+        Intent intent = new Intent(GEOFENCE_BROADCAST_ACTION);
+        PendingIntent pendingIntent = null;
+        pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, intent, 0);
+
+        mLocationClient.addGeoFenceAlert("leap",0.0, 0.0, 100, 1000 * 60, pendingIntent);*/
+
         //启动定位
         mLocationClient.startLocation();
 
     }
 
     private void scanWifi() {
+        SharedPreferences.Editor editor = getSharedPreferences(WIFI_PREF, MODE_PRIVATE).edit();
+
         wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
         wifiManager.startScan();
-        scanWifiResultList = wifiManager.getScanResults();
+        
+        SimpleDateFormat sDateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        String date = sDateFormat.format(new java.util.Date());
 
-        Log.d("chenhao", "total: " + scanWifiResultList.size());
+        scanWifiResultList = wifiManager.getScanResults();
         for (ScanResult scanResult : scanWifiResultList) {
             Log.d("chenhao", scanResult.BSSID + " " + scanResult.SSID + " " + scanResult.level);
+            editor.putInt(scanResult.BSSID + " | " +scanResult.SSID + " | "+date, scanResult.level);
         }
+        editor.commit();
 
-        Log.d("chenhao", "--------------------------");
+        Toast.makeText(this, "scan wifi done", Toast.LENGTH_SHORT).show();
 
     }
 
@@ -126,25 +210,5 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "can't open bluetooth", Toast.LENGTH_SHORT).show();
         }
     }
-
-    private BroadcastReceiver mReceiver = new BroadcastReceiver() {
-
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            // 获得已经搜索到的蓝牙设备
-            if (action.equals(BluetoothDevice.ACTION_FOUND)) {
-                BluetoothDevice device = intent
-                        .getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                int rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI,Short.MIN_VALUE);
-                if (device.getBondState() != BluetoothDevice.BOND_BONDED) {
-                    Log.d("chenhao", device.getName() + " " + device.getAddress() + " " + rssi);
-                }
-            } else if (action.equals(BluetoothAdapter.ACTION_DISCOVERY_FINISHED)) {
-                //Log.d("chenhao","no bluetooth found");
-            }
-        }
-    };
-
 
 }
